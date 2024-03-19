b186c1b42f4adae09621bf8828f345a1af15b542
Ian July <ianjuly4@gmail.com>
2024-03-19 13:43:53 -0600

    badges and schedules lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,27 @@
 def badge_maker(name):
-    return None
-
+    badge = f"Hello, my name is {name}."
+    return badge
+   
 def batch_badge_creator(names):
-    return None
+    badge_list = []
+    for person in names:
+        badge_list.append(f"Hello, my name is {person}.")
+    return badge_list
 
 def assign_rooms(names):
-    return None
+    index = 0
+    rm_assignments = []
+    
+    for person in names:
+        index += 1
+        rm_assignments.append(f"Hello, {person}! You'll be assigned to room {index}!")
+    return rm_assignments
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+
+    for badge in badges:
+        print(badge)
+    for room in room_assignments:
+        print(room)
